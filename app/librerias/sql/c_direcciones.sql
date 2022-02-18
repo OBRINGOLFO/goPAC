e43120fd8cbb008c6088858cc043c83613b32e39
OBRINGOLFO Development <software@obringolfo.com>
2022-02-17 19:38:43 -0600

    OBRINGOLFO Developer Team

---
+++ app/librerias/sql/c_direcciones.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Feb 18, 2022 at 01:29 AM
+-- Server version: 10.2.43-MariaDB
+-- PHP Version: 7.2.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `goadmin_158002F72BF5`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `c_direcciones`
+--
+
+CREATE TABLE `c_direcciones` (
+  `uid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tipo` varchar(2) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'P=principal C=complementaria',
+  `vialidad_tipo` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `vialidad_nombre` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `exterior` varchar(6) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `interior` varchar(6) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `codigo_postal` int(11) DEFAULT NULL,
+  `colonia` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `localidad` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `municipio` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `entidad` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `referencias` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `c_direcciones`
+--
+ALTER TABLE `c_direcciones`
+  ADD PRIMARY KEY (`uid`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
